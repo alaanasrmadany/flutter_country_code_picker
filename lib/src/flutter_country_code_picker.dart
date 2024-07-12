0ad4e986cd697b0278e6c88d84e7ca8db340201b
Your Name <you@example.com>
2024-07-12 18:29:53 +0300

    ..

--- lib/src/flutter_country_code_picker.dart
+++ lib/src/flutter_country_code_picker.dart
@@ -241,7 +241,7 @@ class _IntlPhoneFieldState extends State<IntlPhoneField> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: const EdgeInsetsDirectional.only(bottom: 3, top: 3),
+      padding: const EdgeInsetsDirectional.only(bottom: 5, top: 5),
       width: MediaQuery.of(this.context).size.width,
       decoration: BoxDecoration(
           color: widget.backgroundColor ?? Colors.white,
